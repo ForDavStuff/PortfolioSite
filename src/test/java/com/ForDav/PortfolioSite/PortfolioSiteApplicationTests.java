541f6e854085574ee65a2808cdd81251ac22b979
Dave <69114944+creambastard@users.noreply.github.com>
2022-06-11 15:39:28 +0200

    first_Maven_SetUp (#4)
    inizializzazione Spring, inizio progetto Maven
    
    riorganizzazione del FrontEnd per inserirlo nella cartella /src/main/resources/static

---
+++ src/test/java/com/ForDav/PortfolioSite/PortfolioSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForDav.PortfolioSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
