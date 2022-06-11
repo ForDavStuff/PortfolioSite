aacbf9ab3a27d7069043c9fb8bcc06f5ca1a06cf
Dave <69114944+creambastard@users.noreply.github.com>
2022-06-11 22:00:57 +0200

    aggiunta commenti
    non era commentato e capire era un disastro

--- src/main/java/com/personalForDav/portfolioSite/controller/ControllerBase.java
+++ src/main/java/com/personalForDav/portfolioSite/controller/ControllerBase.java
@@ -13,18 +13,27 @@ import com.personalForDav.portfolioSite.view.ViewIndex;
 @RequestMapping ({"/", "/home"})
 public class ControllerBase {
 
+	//richiama la classe che farà da "assemblatore" della pagina, il tipo formale è abstract
 	private RenderPagina index = null;
 
+	//metodo per richiamare la home con la mappatura della classe
 	@GetMapping
 	public String home() {
+		//ArrayList che si aspetta RenderPagina per trovare i percorsi da cui prendere il testo
 		ArrayList<String> percorsi = new ArrayList<String>();
 		
+		//percorsi che andranno a comporre la pagina richiesta, in questo caso la home
 		percorsi.add("../../../../../../resources/static/html/Header.txt");
 		percorsi.add("../../../../../../resources/static/html/Index.txt");
 
+		//inizializzo index con tipo formale ViewIndex, che sarebbe il tipo concreto
+		//con cui richiamare la Home (attraverso il metodo di RenderPagina
 		index = new ViewIndex();
+		//creo una stringa che sarà letteralmente la pagina HTML
 		String indexHtml = index.renderPagina(percorsi);
-		System.out.println(indexHtml);
+		
+		//cerco di stampare in console la pagina HTML per capire perchè non funziona
+//		System.out.println(indexHtml);
 
 		return indexHtml;
 	}
--- src/main/java/com/personalForDav/portfolioSite/utils/RenderPagina.java
+++ src/main/java/com/personalForDav/portfolioSite/utils/RenderPagina.java
@@ -9,25 +9,26 @@ public abstract class RenderPagina implements IRenderPagina {
 	// metodo per renderizzare l'HTML fino agli script js
 	public String renderHTMLStart(String pagina) {
 
-//		File file = new File("/../../../../../../resources/static/html/htmlStart.txt");
-		File file = new File("/PortFolioSite_Project/src/main/resources/static/html/htmlStart.txt");
+		File file = new File("/../../../../../../resources/static/html/htmlStart.txt");
+//		File file = new File("/PortFolioSite_Project/src/main/resources/static/html/htmlStart.txt");
 //							  /PortFolioSite_Project/src/main/java/com/personalForDav/portfolioSite/utils/RenderPagina.java
 
+		//siccome Lettore restituisce un ArrayList, eseguo un ciclo per farla diventare una stringa unica
 		for (String riga : Lettore.leggiFile(file))
-			pagina += riga;
+			pagina += riga + "\n";
 
 		return pagina;
 	}
 
 	//metodo per caricare gli <script> js da inserire all'interno della pagina
-	//richiamato da renderPagina
+	//richiamato da renderPagina()
+	//uguale a renderHTMLstart(), ma riceve un percorso e una stringa
+		//(da usare dentro a ciclo, per la lista di percorsi)
 	public String renderJS(String percorso, String pagina) {
 
-		File file;
-
-		file = new File(percorso);
+		File file = new File(percorso);
 		for (String riga : Lettore.leggiFile(file)) {
-			pagina += riga;
+			pagina += riga + "\n";
 		}
 		return pagina;
 	}
@@ -37,16 +38,20 @@ public abstract class RenderPagina implements IRenderPagina {
 	@Override
 	public String renderPagina(ArrayList<String> percorsi) {
 
+		//creo una stringa da passare a tutti i metodi che richiamo
 		String pagina = "";
 		File file;		
 
+		//richiamo il metodo che mette <DOCTYPE> etc
 		pagina = renderHTMLStart(pagina);
 
+		//faccio un ciclo per andare a iniettare il JS, usando il metodo .contains()
+		//decidere se salvarne il contenuto
 		for (String unPercorso : percorsi) {
-			while (unPercorso.contains("js")) {
+			if (unPercorso.contains("js")) {
 				file = new File(unPercorso);
 				for (String riga : Lettore.leggiFile(file)) {
-					pagina = renderJS(unPercorso, pagina);
+					renderJS(unPercorso, pagina);
 				}
 			}
 
